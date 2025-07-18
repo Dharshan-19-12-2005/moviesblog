152be22335c00ccd47cd1f48e315fdd1b8c58548
Dharshan-19-12-2005 <dharshan192005@gmail.com>
2025-07-18 14:35:16 +0530

    Blog deployment movies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,8 @@
 import '../styles/globals.css';
-// Removed: import { useRouter } from 'next/router';
-// Removed: import { useEffect } from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  // No more dynamic body class manipulation needed
+ 
 
   return <Component {...pageProps} />;
 }
